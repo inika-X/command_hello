a7fdd8fad72b18799094ce7d261a2d5186e23752
inika_CX <inika20101@gmail.com>
2021-01-21 19:40:49 +0900

    hello.py modified on Local repository

--- hello.py
+++ hello.py
@@ -1,4 +1,7 @@
-print("Hello world")
+//For command line git tutorial
+//Local repository modification
+
+print("Hello World")
 print("Tell Your World")
 print("Tell his world")
 print("Tell her world")
